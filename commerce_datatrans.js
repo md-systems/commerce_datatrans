1133f398b256ea6a8c68b4ee7cb7fc70da9f1936
Sascha Grossenbacher <saschagros@gmail.com>
2015-07-20 11:59:11 +0200

    Improve comments

--- commerce_datatrans.js
+++ commerce_datatrans.js
@@ -3,7 +3,8 @@
   Drupal.behaviors.datatransLightbox = {
     attach: function (context, settings) {
 
-
+      // Register a trigger on the payment button, use once() to avoid
+      // multiple triggers.
       $("#paymentButton", context).once('datatrans', function() {
         $(this).click(function () {
           $("form").submit(function (e) {
