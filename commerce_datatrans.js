53937a2e2329bd5ad6edf856db082ae4410b1829
Vladimir Mitrovic <vladimir.mitrovic@montenasoft.com>
2015-07-28 17:27:37 +0200

    Applying patch 13 from Sascha.

---
+++ commerce_datatrans.js
@@ -0,0 +1,20 @@
+(function ($) {
+
+  Drupal.behaviors.datatransLightbox = {
+    attach: function (context, settings) {
+
+      // Register a trigger on the payment button, use once() to avoid
+      // multiple triggers.
+      $("#paymentButton", context).once('datatrans', function() {
+        $(this).click(function () {
+          $("form").submit(function (e) {
+            e.preventDefault();
+          });
+
+          Datatrans.startPayment({"form": "#paymentButton"});
+        });
+      })
+    }
+  };
+
+})(jQuery);
