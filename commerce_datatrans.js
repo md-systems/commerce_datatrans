70e51b396c324987fdf1408dae03450a3a5a2e3c
Vladimir Mitrovic <vladimir.mitrovic@montenasoft.com>
2015-07-30 05:55:18 +0200

    Module improvements.

--- commerce_datatrans.js
+++ commerce_datatrans.js
@@ -14,11 +14,13 @@
           Datatrans.startPayment({"form": "#paymentButton"});
         });
 
+        // Hides continue button if Datatrans payment method selected.
         if ($('#paymentButton').length) {
           $('#edit-buttons').hide();
         }
       });
 
+      // Shows continue button for any other payment method.
       if (!$('#paymentButton').length) {
         $('#edit-buttons').show();
       }
